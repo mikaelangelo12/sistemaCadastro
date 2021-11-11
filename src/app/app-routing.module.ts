64bafc6c63893d9603aa9771b67c8019fc74b358
Mikael Angelo <mikael@localhost.localdomain>
2021-11-11 16:30:02 -0300

    criando pagina de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './paginas/conteudo/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'cadastro',
+    component: CadastroComponent, pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/paginas/paginas.module.ts
+++ src/app/paginas/paginas.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { CadastroComponent } from './conteudo/cadastro/cadastro.component';
 
 
 
 @NgModule({
   declarations: [
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    BarraLateralComponent,
+    CadastroComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[ CabecalhoComponent, RodapeComponent ]
+  exports:[ CabecalhoComponent, RodapeComponent, BarraLateralComponent]
 })
 export class PaginasModule { }
